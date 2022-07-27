ac3f94bd57194f8a11c0bf569d8ea7530eff7274
Brendon-note <brendoncesa40@gmail.com>
2022-07-26 22:33:44 -0300

    mudar tamanho dos itens do grid

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const container = document.getElementsByClassName("container")
-
 const rows = document.getElementsByClassName("rowElement")
-console.log(rows)
-const GRID_SIZE = 16
 
-for (let i = 0; i < GRID_SIZE; i++) {
-    let row = document.createElement("div")
-    row.classList.add("flex")
-    container[0].appendChild(row)
-    for (let x = 0; x < GRID_SIZE; x++) {
-        let rowElement = document.createElement("div")
-        rowElement.classList.add("rowElement")
-        row.appendChild(rowElement)
+let GRID_SIZE = 16
+// cria o grid
+createGrid(100)
+changeElementSize(100)
+
+function createGrid(GRID_SIZE){
+    for (let i = 0; i < GRID_SIZE; i++) {
+        let row = document.createElement("div")
+        row.classList.add("flex")
+        container[0].appendChild(row)
+        for (let x = 0; x < GRID_SIZE; x++) {
+            let rowElement = document.createElement("div")
+            rowElement.classList.add("rowElement")
+            row.appendChild(rowElement)
+        }
     }
-
 }
-
+// cria um event listener pra cada elemento do grid
 Array.from(rows).forEach((rowItem) => {
-    console.log(rowItem)
     rowItem.addEventListener("mouseover",function(e){
         e.stopPropagation()
         addColor(rowItem)
@@ -25,6 +27,16 @@ Array.from(rows).forEach((rowItem) => {
 });
 
 
+function changeElementSize(GRID_SIZE){
+    let newSize = calcElementWidth(GRID_SIZE)
+    document.documentElement.style.setProperty('--size', newSize);
+}
+
+
 function addColor(rowItem){
     rowItem.classList.add("black")
+}
+
+function calcElementWidth(GRID_SIZE){
+    return `${((480 **2)/(GRID_SIZE ** 2)) ** 0.5}px`   
 }
