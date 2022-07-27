6937146e9b90b5bfb8e6387f785e303adfa3732d
Brendon-note <brendoncesa40@gmail.com>
2022-07-26 23:28:56 -0300

    reset grid feature

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.getElementsByClassName("container")
 const rows = document.getElementsByClassName("rowElement")
+const buttons = document.querySelectorAll("button")
+console.log(buttons)
 
 let GRID_SIZE = 16
 // cria o grid
 createGrid(100)
 changeElementSize(100)
 
+
+buttons[1].addEventListener("click",resetGrid)
 function createGrid(GRID_SIZE){
     for (let i = 0; i < GRID_SIZE; i++) {
         let row = document.createElement("div")
@@ -25,8 +29,7 @@ Array.from(rows).forEach((rowItem) => {
         addColor(rowItem)
     })
 });
-
-
+// muda o tamanho dos elementos do grid
 function changeElementSize(GRID_SIZE){
     let newSize = calcElementWidth(GRID_SIZE)
     document.documentElement.style.setProperty('--size', newSize);
@@ -37,6 +40,17 @@ function addColor(rowItem){
     rowItem.classList.add("black")
 }
 
+// calcula qual tamanho de cada item do grid pra caber numa area de 480x480
 function calcElementWidth(GRID_SIZE){
     return `${((480 **2)/(GRID_SIZE ** 2)) ** 0.5}px`   
+}
+
+function resetGrid(){
+    for (let i = 0; i < GRID_SIZE; i++) {
+        for (const element of rows) {
+            if(element.classList.contains("black")){
+                element.classList.remove("black")
+            }
+        } 
+    }
 }
